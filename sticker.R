d8ebd4576353a41e3f14ac3aac2829bba37ee3a3
Choonghyun Ryu <choonghyun.ryu@gmail.com>
2021-08-03 14:05:14 +0900

    inital

---
+++ sticker.R
@@ -0,0 +1,32 @@
+
+library(hexSticker)
+library(showtext)
+
+font_add(family = "NanumSquare", 
+         regular = "/Users/choonghyunryu/Library/Fonts/NanumSquareRegular.ttf")
+
+sticker("images/detective_2.png", p_family="NanumSquare", 
+        package = "kodlookr", p_size = 8, s_x = 1, s_y = .7, 
+        s_width = 0.4, s_height = 0.2,
+        filename = "images/kodlookr.png")
+
+
+sticker("finder.svg", 
+        package = "kodlookr", p_size = 8, s_x = 1, s_y = .75, 
+        s_width = 0.4, s_height = 0.4,
+        filename = "images/kodlookr.png")
+
+sticker("images/finder_256.png", p_family="NanumSquare", 
+        package = "kodlookr", p_size = 8, s_x = 1, s_y = .75, 
+        s_width = 0.4, s_height = 0.4,
+        filename = "images/kodlookr.png")
+
+
+
+
+sticker("images/detective_2.png", p_family="NanumSquare", 
+        package = "kodlookr", p_size = 8, s_x = 1, s_y = .7, 
+        s_width = 0.4, s_height = 0.2,
+        h_fill="#1b5084", h_color="#193868", 
+        filename = "images/kodlookr.png")
+
